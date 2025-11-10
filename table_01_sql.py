8aa4ee3d1618d97fcc1798b6e55a95aeed8ba07c
Marcos Silveira Gonzalez Júnior <msilveiragjunior@outlook.com>
2025-11-10 17:56:35 -0300

    Commit_03

--- table_01_sql.py
+++ table_01_sql.py
@@ -1,3 +1,21 @@
-import sqlite3
+def create_table(table_name, index, username, email):
+    create_user_table = 'CREATE TABLE {} (' \
+                         '{} INTEGER PRIMARY KEY,' \
+                         '{} TEXT,' \
+                         '{} TEXT,' \
+                         'PRIMARY KEY ({} AUTOINCREMENT)' \
+                        ')'.format(table_name, index, username, email, index)
+    return create_user_table
+# Here we'll insert users into the table. We can consult the results
+# With DB Browser to see the users inserted
 
-# --- Here we'll insert each user to it's table --- #
+
+def insert_user(table_name, username, email, value_username, value_email):
+    insert_user_table = "INSERT INTO {} (" \
+                        "{}, " \
+                        "{}) " \
+                        "VALUES ('{}','{}'" \
+                        ");".format(table_name, username, email,
+                                    value_username,
+                                    value_email)
+    return insert_user_table
--- users.py
+++ users.py
@@ -1,14 +1,48 @@
 import table_01_sql
-# --- Beginning SQL with Python --- #
-# Creating an empty dictionary to create a table for it
-table_01 = {
-}
+import sqlite3
 
-number_of_users = input("Insert the number of users: \n")
+
+# --- Beginning SQL with Python --- #
+
+# --- Here we'll insert each user to it's table --- #
+# Let's create a new table. The module sqllite3 can create
+# a new file with the method connect, that establishes a connection
+# to the db. If the file does not exist, it creates a new one.
+# Or you can use the DB Browser to create it, if you don't want to
+# create the database here.
+connection = sqlite3.connect('users.db')
+
+# We have to create an object called cursor that will allow us
+# to execute SQL commands.
+cursor = connection.cursor()
+
+# Here we'll loop through a the main program, adding the keys
+# and values
 numbers_of_keys = input("Please, specify the number of keys inside each "
                         "user: \n")
-for x in range(0, int(number_of_users)):
-    for i in range(0, int(numbers_of_keys)):
-        key = input("Insira o nome da chave: \n")
-        table_01[key] = input("Insira um valor para a chave: \n")
-print(table_01)
+table_01 = {
+    }
+
+for i in range(0, int(numbers_of_keys)):
+    key = input("Insira o nome da chave: \n")
+    table_01[key] = input("Insira um valor para a chave: \n")
+# Here we add the values to their keys
+keys_dict = list(table_01.keys())
+
+# create_table = table_01_sql.create_table(keys_dict[0], keys_dict[1],
+# #keys_dict[2], keys_dict[3])
+# print(create_table)
+# cursor.execute(create_table)
+
+
+# number_of_users = input("Insert the number of users: \n")
+
+insert_user = table_01_sql.insert_user(keys_dict[0], keys_dict[2],
+                                       keys_dict[3],
+                                       table_01[keys_dict[2]],
+                                       table_01[keys_dict[3]])
+print(insert_user)
+cursor.execute(insert_user)
+connection.commit()
+
+cursor.close()
